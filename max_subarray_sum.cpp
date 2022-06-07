f92c6cda033dcc7ac492ecda77333b5f94524260
skyritzz <ayushisha098@gmail.com>
2022-06-07 11:29:07 +0530

    day3

--- max_subarray_sum.cpp
+++ max_subarray_sum.cpp
@@ -2,7 +2,22 @@
  using namespace std;
 
 
+int solve(int arr[], int n)
+{
+     int sum = 0;
+    int msum = 0;
+    for(int i = 0; i< n; i++){
+        if(sum >= 0)
+        sum += arr[i];
 
+        else
+        sum = arr[i];
+
+        if(sum > msum )
+        msum = sum;
+    }
+    return msum ;
+}
 
  int main()
  {
@@ -16,19 +31,7 @@
          cin>> arr[i];
      }
 
-    int sum = 0;
-    int msum = 0;
-    for(int i = 0; i< n; i++){
-        if(sum >= 0)
-        sum += arr[i];
-
-        else
-        sum = arr[i];
-
-        if(sum > msum )
-        msum = sum;
-    }
-    cout <<  msum << endl;
+    cout<< solve(arr, n) << endl;
 
      return 0;
  }
